afd1e55a4feab9dfedf61da04150a97e261444a1
Lakshay Bomotra <lakshaybomotra@gmail.com>
2025-04-02 02:39:00 +0530

    show generated interviews on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,51 +1,78 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import React from "react";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import {dummyInterviews} from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
+import {
+  getCurrentUser,
+  getInterviewByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/auth.actions";
 
-const Page = () => {
-    return (
-        <>
-            <section className='card-cta'>
-                <div className='flex flex-col gap-6 max-w-lg'>
-                    <h2>
-                        Get Interview-Ready with AI-Powered Practice & Feedback
-                    </h2>
-                    <p className='text-lg'>
-                        Practice on real interview questions & get instant feedbacks
-                    </p>
+const Page = async () => {
+  const user = await getCurrentUser();
 
-                    <Button asChild className='btn-primary max-sm:w-full'>
-                        <Link href='/interview'>Start an Interview</Link>
-                    </Button>
-                </div>
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewByUserId(user?.id!),
+    await getLatestInterviews({userId: user?.id!})
+  ])
 
-                <Image src='/robot.png' alt='robo-dude' width={400} height={400} className="max-sm:hidden"/>
-            </section>
+  const hasPastInterviews = (userInterviews ?? []).length > 0;
+  const hasUpcomingInterviews = (latestInterviews ?? []).length > 0;
 
-            <section className='flex flex-col gap-6 mt-8'>
-                <h2>Your Interviews</h2>
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedbacks
+          </p>
 
-                <div className="interviews-section">
-                    {dummyInterviews.map((interview) => (
-                        <InterviewCard {...interview} key={interview.id}/>
-                    ))}
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
 
-                    {/*<p>You haven&apos;t taken any interviews yet</p>*/}
-                </div>
-            </section>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
 
-            <section className='flex flex-col gap-6 mt-8'>
-                <h2>Take an Interview</h2>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
 
-                <div className='interviews-section'>
-                    <p>There are no interviews available</p>
-                </div>
-            </section>
-        </>
-    )
-}
+        <div className="interviews-section">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
+        </div>
+      </section>
 
-export default Page
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+
+        <div className="interviews-section">
+        {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>There are no new interviews available</p>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
